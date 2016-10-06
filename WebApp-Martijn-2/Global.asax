a0b89bda7797e1c460080b38345054d971db6270
MartijndeKam <epo2600@gmail.com>
2016-10-06 13:28:10 +0200

    Add project files.

---
+++ WebApp-Martijn-2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApp_Martijn_2.MvcApplication" Language="C#" %>
---
+++ WebApp-Martijn-2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApp_Martijn_2.Startup))]
+namespace WebApp_Martijn_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
